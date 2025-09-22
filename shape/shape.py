bfd8bf6160191dfeb1b65595aa6867ee5ddcd05b
Klima2 <klima2@rrc.ca>
2025-09-22 18:42:26 -0500

    Removed the @propety. The diagram doesn't show this method.

--- shape/shape.py
+++ shape/shape.py
@@ -12,24 +12,41 @@ class Shape(ABC):
     args:
         color (str): The color of the shape.
     attributes:
-        _color (str): The color of the shape.
+        _color (str): The color of the shape. Protected attribute.
     methods:
+        init__(color: str): Initializes the shape with a color.
+        abstract area() -> float: Abstract method to compute the 
+        area of the shape.
+        abstract perimeter() -> float: Abstract method to compute 
+        the perimeter of the shape.
         color() -> str: Accessor for the color of the shape.
+    raises:
+        ValueError: If the color is not a string.
+    examples:
+        >>> shape = Shape("red")
+        >>> shape.color()
+        'The color shape i red.'
+    returns:
+        Color shape is {color}.
     """
     
 
     def __init__(self, color: str):
-        """Initialize the shape with a color."""
-        if isinstance(color, str):
+        """Initialize the shape with a color.
+        Args:
+            color (str): The color shape. 
+            Raises:
+                ValueError: If the color cannot be blank."""
+        if len(color.strip()) > 0:
             self._color = color
         else:
             raise ValueError("Color cannot be blank.")
+    
+    def __str__(self):
+        """String returns the color shape."""
 
-    
-    def color(self) -> str:
-        """Accessor for the color of the shape."""
-        return (f"The color shape is {self._color}.\n")
-    
+        return super().__str__(f"The color shape is {self._color}.\n")
+
     @abstractmethod
     def area(self) -> float:
         """Abstract method to compute the area of the shape."""
