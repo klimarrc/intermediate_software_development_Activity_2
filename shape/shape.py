54ece57eecacd5d9145c18f74bfa938df0ff0c6a
Klima2 <klima2@rrc.ca>
2025-09-22 19:10:52 -0500

    Started subclass triangle.

--- shape/shape.py
+++ shape/shape.py
@@ -7,51 +7,41 @@ from abc import ABC, abstractmethod
 
 
 class Shape(ABC):
-    """Abstract base class for geometric shapes.
-
-    args:
-        color (str): The color of the shape.
-    attributes:
-        _color (str): The color of the shape. Protected attribute.
-    methods:
-        init__(color: str): Initializes the shape with a color.
-        abstract area() -> float: Abstract method to compute the 
-        area of the shape.
-        abstract perimeter() -> float: Abstract method to compute 
-        the perimeter of the shape.
-        color() -> str: Accessor for the color of the shape.
-    raises:
-        ValueError: If the color is not a string.
-    examples:
-        >>> shape = Shape("red")
-        >>> shape.color()
-        'The color shape i red.'
-    returns:
-        Color shape is {color}.
-    """
-    
+    """ class Shape is an  Abstract class for geometric shapes."""
 
     def __init__(self, color: str):
         """Initialize the shape with a color.
         Args:
             color (str): The color shape. 
-            Raises:
-                ValueError: If the color cannot be blank."""
+
+        Raises:
+                ValueError: when the color cannot be blank.
+        """
+
         if len(color.strip()) > 0:
             self._color = color
         else:
             raise ValueError("Color cannot be blank.")
     
-    def __str__(self):
-        """String returns the color shape."""
-
-        return super().__str__(f"The color shape is {self._color}.\n")
-
+    def __str__(self) -> str:
+        """String returns the color shape.
+        Returns:
+            str: A string representation of the color shape 
+            with the message - "Color shape is {color}."
+        """
+        return (f"Color shape is {self._color}.")
+    
     @abstractmethod
     def area(self) -> float:
-        """Abstract method to compute the area of the shape."""
+        """Abstract method to compute the area of the shape in 
+        centimeters.
+    """
         pass
+
     @abstractmethod
     def perimeter(self) -> float:
-        """Abstract method to compute the perimeter of the shape."""
+        """Abstract method to compute the perimeter of the shape 
+        in centimeters.
+        """
+        
         pass
--- shape/triangle.py
+++ shape/triangle.py
@@ -1,4 +1,31 @@
 """This module defines the Triangle class."""
 
-__author__ = ""
-__version__ = ""
+__author__ = "Kailine Lima" 
+__version__ = "1.0.0"
+
+from shape.shape import Shape
+
+
+class Triangle(Shape):
+    """ class Triangle is a subclass of super classShape for triangles."""
+
+    def __init__(self, color: str, side_1: int, side_2: int, side_3: int,
+        ):
+        """Initialize the triangle with a color, base, height and sides.
+        Args:
+            color (str): The color shape. 
+            side_1 (int): The length of side 1 of the triangle in centimeters.
+            side_2 (int): The length of side 2 of the triangle in centimeters.
+            side_3 (int): The length of side 3 of the triangle in centimeters.
+        Raises:
+                ValueError: when the color cannot be blank,
+                when side_1 is not numeric.
+                when side_2 is not numeric.
+                when side_3 is not numeric.
+        """
+
+        super().__init__(color)
+        if isinstance(side_1, (int)):
+            self._side_1 = side_1
+        else:
+            raise ValueError("Side 1 must be a number greater than zero.")
