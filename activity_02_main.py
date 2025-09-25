274132ebcb83e9a6633f941771c8a2bae1072804
Klima2 <klima2@rrc.ca>
2025-09-24 21:28:36 -0500

    Added the client program with Package Initialization, and I followed the instruction by using the try/except block for any method that raise an exception.

--- activity_02_main.py
+++ activity_02_main.py
@@ -8,13 +8,14 @@ __credits__ = "Kailine Lima"
 
 from shape import *
 
+
 def main():
     """Test the functionality of the methods encapsulated 
     in this project.
     """
 
-    # In the statements coded below, ensure that any statement that 
-    # could result in an exception is handled.  When exceptions are 
+    # In the statements coded below, ensure that any statement that
+    # could result in an exception is handled.  When exceptions are
     # 'caught', display the exception message to the console.
 
     # *** PART 1 ***
@@ -23,7 +24,7 @@ def main():
     # 1. Create an empty list of Shape objects.
     shapes = []
 
-    # 2. Code a statement which creates an instance of the Triangle 
+    # 2. Code a statement which creates an instance of the Triangle
     # class.
     # Append the Triangle to the list of shapes.
     try:
@@ -31,7 +32,7 @@ def main():
         shapes.append(triangle)
     except Exception as e:
         print(e)
-    # 3. Code a statement which creates an instance of the Rectangle 
+    # 3. Code a statement which creates an instance of the Rectangle
     # class.
     # Append the Rectangle to the list of shapes.
     try:
@@ -40,7 +41,7 @@ def main():
     except Exception as e:
         print(e)
 
-    # 4. Code 3 additional statements which creates an instance of 
+    # 4. Code 3 additional statements which creates an instance of
     # Triangle or Rectangle classes (your choice).
     # Append these instances to the list of shapes.
     try:
@@ -48,14 +49,30 @@ def main():
         shapes.append(triangle2)
     except Exception as e:
         print(e)
+    try:
+        rectangle2 = Rectangle("Green", 4, 7)
+        shapes.append(rectangle2)
+    except Exception as e:
+        print(e)
+    try:
+        triangle3 = Triangle("Yellow", 6, 8, 10)
+        shapes.append(triangle3)
+    except Exception as e:
+        print(e)
 
-
-    # 5. Iterate through the list of shapes.  
+    # 5. Iterate through the list of shapes.
     # On each iteration:
     # - print the shape
     # - print the area of the shape to 2 decimal places
     # - print the perimeter of the shape to 2 decimal places
 
+    for shape in shapes:
+        try:
+            print(shape)
+            print(f"Area: {shape.calculate_area():.2f} square centimeters.")
+            print(f"Perimeter: {shape.calculate_perimeter():.2f} centimeters.")
+        except Exception as e:
+            print(e)
     # *** END PART 1 ***
 
 
