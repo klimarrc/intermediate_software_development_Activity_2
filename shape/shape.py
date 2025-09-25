0b9fa44d110e34ba14844d26af3eca3c6573dd71
Klima2 <klima2@rrc.ca>
2025-09-25 09:35:54 -0500

    Reviewed docstrings and formmat PEP8 in the shape.py.

--- shape/shape.py
+++ shape/shape.py
@@ -1,4 +1,12 @@
-"""This module defines the Shape class."""
+"""
+This module defines the Shape class:
+- Shape is an superclass for geometric shapes.
+- It includes attribute color that represents string value.
+- import ABC and abstractmethod from abc module.
+- It has an abstract method calculate_area  and calculate_perimeter to 
+compute the area of the shape in centimeters.
+- It has a __str__ method that returns message "Color shape is {color}."
+"""
 
 __author__ = "Kailine Lima"
 __version__ = "1.0.0"
@@ -11,13 +19,14 @@ class Shape(ABC):
 
     def __init__(self, color: str):
         """Initialize class attributes to correspond arguments values.
+
         Args:
             color (str): The color shape. 
 
         Raises:
                 ValueError: when the color cannot be blank.
         """
-
+        
         if len(color.strip()) > 0:
             self._color = color
         else:
@@ -25,6 +34,7 @@ class Shape(ABC):
 
     def __str__(self) -> str:
         """String returns the color shape.
+        
         Returns:
             str: A string representation of the color shape 
             with the message - "Color shape is {color}."
@@ -35,7 +45,7 @@ class Shape(ABC):
     def calculate_area(self) -> float:
         """Abstract method to compute the area of the shape in 
         centimeters.
-    """
+        """
         pass
 
     @abstractmethod
